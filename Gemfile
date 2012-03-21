e273b6cd214ca1692b7c8782909a2e60492d1695
Travis Valentine <travis.valentine@livingsocial.com>
2012-03-21 08:57:38 -0400

    Add simplecov and guard

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source :rubygems
 
 gem "rspec"
+gem "rspec"
+gem "guard"
+gem "guard-rspec"
+gem 'simplecov', :require => false, :group => :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
 require './customer'
 require './item'
 require './invoice'
