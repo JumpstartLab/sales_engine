cef1c6b31bd8aead1bce2275f083f570a141cd2a
es and es <andrew.thal+athal7+marktabler@livingsocial.com>
2012-03-21 19:16:51 -0400

    finish searchable spec

--- spec/searchable_spec.rb
+++ spec/searchable_spec.rb
@@ -17,13 +17,13 @@ CLASSES.each do |klass, attribute|
 		end
 
 		describe ".find_by_id('7')" do
-	    it "returns one instance of #{klass} with ID '7' " do
+			it "returns one instance of #{klass} with ID '7' " do
 				klass.send(:find_by_id, '7').id.should == '7'
 			end
 		end
 
-	  describe ".find_all_by_id('10')" do
-			it "returns the instances of #{klass} with ID '10' " do
+		describe ".find_all_by_id('10')" do
+			it "returns all instances of #{klass} with ID '10' " do
 				klass.send(:find_all_by_id, '10').map(&:id).should == ['10']
 			end
 			it "doesn't miss any records with id of '10'" do
@@ -38,6 +38,30 @@ CLASSES.each do |klass, attribute|
 				expect { klass.send(:find_by_foo_bar, '42') }.should raise_error(NoMethodError)
 			end
 		end
+	end
+end
 
+
+describe SalesEngine::Customer do
+	describe ".find_all_by_first_name" do
+		it "returns all instances of Customer with first_name Bobbie" do
+			SalesEngine::Customer.find_all_by_first_name("Bobbie").size.should == 2
+			SalesEngine::Customer.find_all_by_first_name("Bobbie").map(&:first_name).uniq.should == ["Bobbie"]
+		end
+		it "is case insensitive" do
+			SalesEngine::Customer.find_all_by_first_name("bobbie").size.should == 2
+			SalesEngine::Customer.find_all_by_first_name("bobbie").map(&:first_name).uniq.should == ["Bobbie"]
+		end
+	end
+
+	describe ".find_all_by_last_name" do
+		it "returns all instances of Customer with last_name Johnsons" do
+			SalesEngine::Customer.find_all_by_last_name("Johnson").size.should == 5
+			SalesEngine::Customer.find_all_by_last_name("Johnson").map(&:last_name).uniq.should == ["Johnson"]
+		end
+		it "is case insensitive" do
+			SalesEngine::Customer.find_all_by_last_name("johnson").size.should == 5
+			SalesEngine::Customer.find_all_by_last_name("johnson").map(&:last_name).uniq.should == ["Johnson"]
+		end
 	end
 end
