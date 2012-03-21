b7150f68489ad615d71ae58816681ee9024c89bc
Melanie Gilman <melanie.gilman@gmail.com>
2012-03-21 12:49:50 -0400

    modified sales engine

--- sales_engine.rb
+++ sales_engine.rb
@@ -18,15 +18,23 @@ class SalesEngine
                 :merchants
 
   def initialize
-    ds.data_load
+    self.customers = load_file("customers.csv").collect {|line| Customer.new(line)}
+    self.invoices = load_file("invoices.csv").collect {|line| Invoice.new(line)}   
+    self.merchants = load_file("merchants.csv").collect {|line| Merchant.new(line)}
+    self.items = load_file("items.csv").collect {|line| Item.new(line)}
+    self.invoice_items = load_file("invoice_items.csv").collect {|line| InvoiceItem.new(line)}
+    self.transactions = load_file("transactions.csv").collect {|line| Transaction.new(line)}
   end
 
-
+  def load_file(filename)
+    CSV.open(filename, OPTIONS)
+  end
 end
 
 
 
 se = SalesEngine.new
+cu = Customer.new()
 #se.customers.random
 #customer_instance = Customer.new(customers)
 #customer_instance.random
