634647f31587d326b592d37f359e520d3da61c88
Jonan Scheffler <jonanscheffler@gmail.com>
2012-03-21 13:41:43 -0400

    Git ignore setup.

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,9 +1,13 @@
 class Merchant
-  def initialize(attributes)
-    id = attributes[:id]
-    name = attributes[:name]
-    error_msg = 'Merchants must have an id and name'
+  attr_reader :created_at
+  attr_reader :id
 
+  def initialize(attributes)
+    @id = attributes[:id]
+    name = attributes[:name]
+    @created_at = parse_date(attributes[:created_at])
+
+    error_msg = 'Merchants must have an id and name'
     raise ArgumentError, error_msg unless valid_id?(id) and valid_name?(name)
   end
 
@@ -15,5 +19,10 @@ class Merchant
 
   def valid_name?(name)
     true unless name.to_s.empty?
+  end
+
+  def parse_date(date)
+    date = DateTime.now.to_s if date.to_s.empty?
+    DateTime.parse date
   end
 end
--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -1,8 +1,9 @@
 require './spec/spec_helper'
 
 describe Merchant do
+  let(:merchant) {Merchant.new(:id => 1, :name => "Test Merchant")}
+
   it 'creates a merchant with valid attributes' do
-    merchant = Merchant.new(:id => 1, :name => "Test Merchant")
     merchant.nil?.should be_false
   end
 
@@ -11,6 +12,12 @@ describe Merchant do
       Merchant.new(:id => nil, :name => "Test Merchant") 
     end.to raise_error(ArgumentError)
   end
+
+  it "has an id" do
+    merchant.id.should_not be_nil
+  end
+
+  it "has a name"
 
   it "doesn't create a merchant with a blank id" do
     expect do
@@ -41,4 +48,20 @@ describe Merchant do
       Merchant.new() 
     end.to raise_error(ArgumentError)
   end
+
+  it "responds to created_at" do
+    merchant.should be_respond_to(:created_at)  
+  end
+
+  it "returns a DateTime object when created_at is called" do
+    merchant.created_at.class.should == DateTime
+  end
 end
+
+
+
+
+
+
+
+
