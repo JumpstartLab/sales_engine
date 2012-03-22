6e2a069ee5efabef4a9ac9c93dc43005e7f9511e
Mary Cutrali and Mike Chlipala <mchlipala+maryelizbeth+mikesea@gmail.com>
2012-03-22 14:25:51 -0400

    Customer tests

--- spec/sales_engine/customer_spec.rb
+++ spec/sales_engine/customer_spec.rb
@@ -1,1 +1,30 @@
 require 'spec_helper'
+
+describe SalesEngine::Customer do 
+
+  describe ".random" do 
+    it "returns a random instance of Customer" do
+      result = Customer.new 
+      result.class.should == Customer.class 
+    end 
+  end
+
+  describe "#invoices" do 
+    it "returns a collection of invoice instances for a specific customer" do 
+      result == Customer.new 
+      result.invoices.class.should == invoice.class 
+    end 
+  end 
+
+  describe "#transactions" do 
+    it "returns an array of transaction instances for a specific customer" do
+      result == Customer.new 
+      result.transactions.class.should == transaction.class
+    end 
+  end 
+
+  describe "#favorite_merchant" do 
+    it "returns an instance of the merchant at which customer has most transactions" do 
+    end
+  end
+end
