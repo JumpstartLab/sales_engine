a1cc400f32478cdb5a9c2e6014de91770fd5ab18
Andrewglass1 <Andrewglass1@gmail.com>
2012-03-22 10:45:52 -0400

    growl?

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,4 +1,4 @@
-require './data_store'
+#require './data_store'
 require './search'
 
 class Customer
@@ -26,47 +26,46 @@ class Customer
 
   def self.random
       self.customers.sample
-      puts self.customers.sample
   end
 
   def self.find_by_id(match)
-    puts Search.find_all_by("id", match, self.customers).sample.inspect
+    Search.find_all_by("id", match, self.customers).sample
   end
 
   def self.find_all_by_id(match)
-    puts Search.find_all_by("id", match, self.customers).inspect
+    Search.find_all_by("id", match, self.customers)
   end
 
   def self.find_by_first_name(match)
-    puts Search.find_all_by("first_name", match, self.customers).sample.inspect
+    Search.find_all_by("first_name", match, self.customers).sample
   end
 
   def self.find_all_by_first_name(match)
-    puts Search.find_all_by("first_name", match, self.customers).inspect
+    Search.find_all_by("first_name", match, self.customers)
   end
 
   def self.find_by_last_name(match)
-    puts Search.find_all_by("last_name", match, self.customers).sample.inspect
+    Search.find_all_by("last_name", match, self.customers).sample
   end
 
   def self.find_all_by_last_name(match)
-    puts Search.find_all_by("last_name", match, self.customers).inspect
+    Search.find_all_by("last_name", match, self.customers)
   end
 
   def self.find_by_updated_at(match)
-    puts Search.find_all_by("updated_at", match, self.customers).sample.inspect
+    Search.find_all_by("updated_at", match, self.customers).sample
   end
 
   def self.find_all_by_updated_at(match)
-    puts Search.find_all_by("updated_at", match, self.customers).inspect
+    Search.find_all_by("updated_at", match, self.customers)
   end
 
   def self.find_by_created_at(match)
-    puts Search.find_all_by("created_at", match, self.customers).sample.inspect
+    Search.find_all_by("created_at", match, self.customers).sample
   end
 
   def self.find_all_by_created_at(match)
-    puts Search.find_all_by("created_at", match, self.customers).inspect
+    Search.find_all_by("created_at", match, self.customers)
   end
 end
 
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,13 +1,16 @@
-require './customer.rb'
+require './lib/customer.rb'
 
 describe Customer do
+  before(:all) do
+    let(:a) { Customer.new({id=>"3"}) }
+    let(:b) { Customer.new({}) }
+    let(:c) { Customer.new({}) }
+    let(:d) { Customer.new({}) }
+    let(:e) { Customer.new({}) }
+  end
+
 
   describe ".random" do
-    a = Customer.new({})
-    b = Customer.new({})
-    c = Customer.new({})
-    d = Customer.new({})
-    e = Customer.new({})
     it "returns a customer" do
       Customer.random.class.should == Customer
     end
@@ -16,14 +19,26 @@ describe Customer do
       Customer.random.should_not == Customer.random
     end
   end
-  describe ".find_by_cust_id" do
-    a = Customer.new({:cust_id=>3})
+
+  describe ".find_by_id" do
+
     it "returns a customer" do
-      Customer.find_by_cust_id(3).class.should == Customer
+      Customer.find_by_id("3").class.should == Customer
     end
-    
-    it "returns different customers on two calls" do
-      Customer.find_by_cust_id(3).should == a
+
+    it "returns the correct customer id" do
+      Customer.find_by_id("3").id.should == "3"
+    end
+
+    it "returns the correct customer" do
+      Customer.find_by_id("3").should == a
+    end
+  end
+
+  describe ".find_all_by_id" do
+
+    it "returns a array" do
+      Customer.find_all_by_id("3").class.should == Array
     end
   end
 end
