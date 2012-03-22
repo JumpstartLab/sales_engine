8f35f9320f5fb680dbebd31995a84b6db416d0fc
Chris Anderson <christopher.anderson@gmail.com>
2012-03-22 10:07:35 -0400

    Fixed load paths and such

--- lib/sales_engine/load.rb
+++ lib/sales_engine/load.rb
@@ -5,7 +5,5 @@ require 'sales_engine/invoice_item'
 require 'sales_engine/item'
 require 'sales_engine/merchant'
 require 'sales_engine/transaction'
-
-class SalesEngine
-end
+require 'sales_engine/sales_engine'
 
---
+++ lib/sales_engine/sales_engine.rb
@@ -0,0 +1,5 @@
+class SalesEngine
+  def self.startup
+    puts "starting up!"
+  end
+end
