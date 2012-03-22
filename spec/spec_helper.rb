ea8b16a44ca22967db519977e96670ce0d63fa9a
es and es <andrew.thal+athal7+marktabler@livingsocial.com>
2012-03-22 09:36:58 -0400

    Fix simplecov load order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
 require 'simplecov'
-require 'bundler'
-require './lib/sales_engine'
-
-Bundler.require
-
 SimpleCov.start do
 	add_filter "/spec/"
 end
 
+require 'bundler'
+Bundler.require
+
+require './lib/sales_engine'
 SalesEngine.startup
