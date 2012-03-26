08a59d55fdb0c18d3859a10cb188b504c5d14c7c
michael verdi <michael.v.verdi@gmail.com>
2012-03-25 22:01:10 -0400

    added tests for initialize method for customer class

--- spec/sales_engine/customer_spec.rb
+++ spec/sales_engine/customer_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 
 describe SalesEngine::Customer do
+  let(:customer) {SalesEngine::Customer.new({:id => 1, :first_name => "john", :last_name => "ben", :created_at => "time", :updated_at => "time"})}
   describe 'initialize' do
     context "when instantiating a new customer" do
-    let(:customer) {SalesEngine::Customer.new({:id => 1, :first_name => "john", :last_name => "ben", :created_at => "time", :updated_at => "time"})}
       it "sets id" do
-        customer.id.should == 1
+        customer.id.should_not be_nil
+      end
+      it 'sets first_name' do
+        customer.first_name.should_not be_nil
+      end
+      it 'sets last_name' do
+        customer.last_name.should_not be_nil
+      end
+      it 'sets created_at' do
+        customer.created_at.should_not be_nil
+      end
+      it 'sets updated_at' do
+        customer.updated_at.should_not be_nil
       end
     end
   end
-  
 end
