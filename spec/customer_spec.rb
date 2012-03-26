83e52586d18b370e7236bfaaf6ba94908f56b66e
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-03-26 11:28:22 -0400

    Invoice changed to use fake

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe SalesEngine::Customer do
   describe "#invoices" do
-    let(:customer) { SalesEngine::Customer.new(1, "", "", Date.today, Date.today) }
+    let(:customer) { Fabricator(:customer, :id => 1) }
     let(:invoice) { mock(SalesEngine::Invoice) }
     let(:invoice2) { mock(SalesEngine::Invoice) }
     let(:other_invoice) { mock(SalesEngine::Invoice) }
@@ -15,21 +15,21 @@ describe SalesEngine::Customer do
     end
     
     context "when customer has multiple invoices" do
-      let(:customer) { SalesEngine::Customer.new(1, "", "", Date.today, Date.today) }
+      let(:customer) { Fabricator(:customer, :id => 1) }
       it "returns an array of invoices with matching invoice_id" do
         customer.invoices.should == [invoice, other_invoice]
       end
     end
 
     context "when customer has one invoice" do
-      let(:customer) { SalesEngine::Customer.new(2, "", "", Date.today, Date.today) }
+      let(:customer) { Fabricator(:customer, :id => 2) }
       it "returns an array with one invoice" do
         customer.invoices.should == [invoice2]
       end
     end
 
     context "when customer has no invoices" do
-      let(:customer) { SalesEngine::Customer.new(3, "", "", Date.today, Date.today) }
+      let(:customer) { Fabricator(:customer, :id => 3) }
       it "returns an empty array" do
         customer.invoices.should == []
       end
