a4eab41421757e852d3a9ae16adb10fe1524a916
michael verdi <michael.v.verdi@gmail.com>
2012-03-25 22:53:33 -0400

    added two pending and one passing test for the customer invoice method

--- spec/sales_engine/customer_spec.rb
+++ spec/sales_engine/customer_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe SalesEngine::Customer do
-  let(:customer) {SalesEngine::Customer.new({:id => 1, :first_name => "john", :last_name => "ben", :created_at => "time", :updated_at => "time"})}
+  param = {:id => 1, :first_name => "Eliezer", :last_name => "Lemke",
+           :created_at => "2012-02-26 20:56:56 UTC",
+           :updated_at => "2012-02-26 20:56:56 UTC"}
+  let(:customer) {SalesEngine::Customer.new(param)}
   describe 'initialize' do
     context "when instantiating a new customer" do
       it 'receives a hash as a param' do
-        param = {:id => 1, :first_name => "john", :last_name => "ben", :created_at => "time", :updated_at => "time"}
         param.should be_a(Hash)
       end
       [:id, :first_name, :last_name, :created_at, :updated_at].each do |method|
@@ -28,4 +30,12 @@ describe SalesEngine::Customer do
       collection.should_not be_nil
     end
   end
+
+  describe '#invoices' do
+    it "returns an array" do
+      customer.invoices.should be_a(Array)
+    end
+    it 'returns instances of the invoice class'
+  end
+  it 'returns 0 if no matches are found'
 end
