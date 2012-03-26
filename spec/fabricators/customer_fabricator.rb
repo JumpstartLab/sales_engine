f64d30d2e772ad9723d7bfacdcdb715d8d737112
Travis Valentine and Michael Verdi <tvalent2+travisvalentine+verdi327@gmualumni.org>
2012-03-26 11:42:47 -0400

    Comment out fabricator

--- spec/fabricators/customer_fabricator.rb
+++ spec/fabricators/customer_fabricator.rb
@@ -1,3 +1,7 @@
 Fabricator(:customer, :class_name => "SalesEngine::Customer") do
   id "1"
+  first_name "Eliezer"
+  last_name "Lemke"
+  created_at "2012-02-26 20:56:56 UTC"
+  updated_at "2012-02-26 20:56:56 UTC"
 end
