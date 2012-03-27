e3d21429f96b2a913a7f7e2bd4062f5aaec8b95b
Matt Yoho <mby@mattyoho.com>
2012-03-27 12:23:25 -0400

    Update Customer spec

--- spec_evaluation/customer_spec.rb
+++ spec_evaluation/customer_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe Customer do
-  let(:customer) { Customer.find_by_id 999 }
+describe SalesEngine::Customer do
+  let(:customer) { SalesEngine::Customer.find_by_id 999 }
 
   context "Relationships" do
     describe ".random" do
       it "usually returns different things on subsequent calls" do
-        customer_one = Customer.random
+        customer_one = SalesEngine::Customer.random
         10.times do
-          customer_two = Customer.random
+          customer_two = SalesEngine::Customer.random
           break if customer_one != customer_two
         end
 
@@ -17,12 +17,12 @@ describe Customer do
     end
 
     describe ".find_by_last_name" do
-      customer = Customer.find_by_last_name "Arjun"
-      customer.first_name.should == "Howell"
+      customer = SalesEngine::Customer.find_by_last_name "Ullrich"
+      customer.first_name.should == "Ramon"
     end
 
     describe ".find_all_by_first_name" do
-      customers = Customer.find_all_by_first_name "Hauck"
+      customers = SalesEngine::Customer.find_all_by_first_name "Sasha"
       customers.should have(2).customers
     end
   end
@@ -30,7 +30,7 @@ describe Customer do
   context "Relationships" do
     describe "#invoices" do
       it "returns all of a customer's invoices" do
-        customer.invoices.should have(5).invoices
+        customer.invoices.should have(7).invoices
       end
 
       it "returns invoices belonging to the customer" do
@@ -43,17 +43,17 @@ describe Customer do
   end
 
   context "Business Intelligence" do
-    let(:customer) { Customer.find_by_id 2 }
+    let(:customer) { SalesEngine::Customer.find_by_id 2 }
 
     describe "#transactions" do
       it "returns a list of transactions the customer has had" do
-        customer.transactions.should have(7).transactions
+        customer.transactions.should have(1).transaction
       end
     end
 
     describe "#favorite_merchant" do
       it "returns the merchant where the customer has had the most transactions" do
-        customer.favorite_merchant.name.should == "Altenwerth and Sons"
+        customer.favorite_merchant.name.should == "Shields, Hirthe and Smith"
       end
     end
   end
