d3073367dcd80039c8e9cde977f9eba99d227fea
Jonan Scheffler <jonanscheffler@gmail.com>
2012-03-28 09:03:06 -0400

    Convert object relations to object_ids

--- lib/sales_engine/invoice_item.rb
+++ lib/sales_engine/invoice_item.rb
@@ -4,13 +4,12 @@ module SalesEngine
   class InvoiceItem
     include Model
 
-    attr_reader :item, :invoice, :quantity, :unit_price
+    attr_reader :item_id, :invoice_id, :quantity, :unit_price
 
     def initialize(attributes)
       super(attributes)
-
-      @item = attributes[:item]
-      @invoice = attributes[:invoice]
+      @item_id = clean_integer(attributes[:item_id])
+      @invoice_id = clean_integer(attributes[:invoice_id])
       @quantity = clean_integer(attributes[:quantity])
       @unit_price = clean_float(attributes[:unit_price])
 
