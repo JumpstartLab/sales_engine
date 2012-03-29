4c9f22ca659237128a80890cf1428d353ec66065
Chris Anderson <christopher.anderson@gmail.com>
2012-03-28 23:33:25 -0400

    Refactored to gem layout

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,3 +1,5 @@
+$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(File.dirname(__FILE__)),'lib'))).uniq!
+
 require 'sales_engine/customer'
 require 'sales_engine/database'
 require 'sales_engine/invoice'
@@ -5,5 +7,10 @@ require 'sales_engine/invoice_item'
 require 'sales_engine/item'
 require 'sales_engine/merchant'
 require 'sales_engine/transaction'
-require 'sales_engine/sales_engine'
+require 'sales_engine/model'
 
+class SalesEngine
+  def self.startup
+    puts "starting up!"
+  end
+end
